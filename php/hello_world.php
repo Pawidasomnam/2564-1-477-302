ae993c759ba8fd1e634031833e4ed401cab58985
Pawidasomnam <pawida.8402@gmail.com>
2021-10-26 23:49:49 +0700

    hello

---
+++ php/hello_world.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Geolocation</title>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <title>Hello world in PHP</title>
+</head>
+<body>
+    <?php
+      $txt ="Hello world";
+      $txt1 ="I am";
+      $age=40;
+      $txt2="years old.";
+      echo $txt1 . $age . $txt2;
+      //$chk = true;
+      //if($chk):
+        //else{}
+      
+      //echo "<div style='color:red'><h1>Helo world in php</h1></div>";
+      ?>
+</body>
+</html>
---
+++ php/hello_world2.php
@@ -0,0 +1,2 @@
+<?php echo "<div style='color:red'><h1>Helo world in php</h1></div>"; ?>
+<div style="color:blue"></div>
